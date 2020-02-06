9adf7ba8e3cf87a1bcae5ba290138e4815f1d2a5
alumno <alumno@utnfra.com.ar>
2020-02-05 22:14:45 -0300

    Se resuelven ejercicios 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,8 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+    var dato;
+    dato =prompt("ingrese algo"), id= numero;
+    alert("numero");
+
 }
-
-
