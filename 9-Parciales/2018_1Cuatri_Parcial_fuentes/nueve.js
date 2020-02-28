06dd27c583c90e5a2e6f8ae641c3eafcce4e1956
alumno <alumno@utnfra.com.ar>
2020-02-27 22:02:14 -0300

    ejercicio 9 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,79 @@
+var marca;
+var peso;
+var temperatura;
+var temperaturaPar = 0;
+var temperaturaMenorCero = 0;
+var acumuladorPeso = 0;
+var marcaMasPesada;
+var maxPeso;
+var minPeso;
+var respuesta = true;
+var bandera = true;
+var contador = 0;
+
 function mostrar()
 {
-
+    do
+    {   
+        
+        marca = prompt("Ingrese la marca del producto");
+       
+        while (marca == "")
+        {
+        marca = prompt("Ingrese una marca correcta"); 
+        }
+       
+        do
+        {
+            peso = prompt("Ingrese el peso entre 1 y 100");  
+            peso = parseInt(peso);
+        }while(isNaN(peso) || peso >100 || peso <1 );
+    acumuladorPeso = acumuladorPeso + peso;
+        do
+        {
+            temperatura = prompt("ingrese la temperatura entre -30 y 30");
+            temperatura = parseInt(temperatura);
+        }while(isNaN(temperatura) || temperatura <-30 || temperatura >30);
+        if (temperatura %2 ==0 &&temperatura!=0)
+        {
+            temperaturaPar++;
+        }
+        if (bandera)
+        {
+            marcaMasPesada = marca;
+            minPeso = peso;
+            maxPeso = peso;
+            bandera= false
+        }
+        else
+        {
+            if (peso >maxPeso)
+            {
+                marcaMasPesada = marca;
+                maxPeso= peso;
+            }
+            else
+            {
+                if (peso < minPeso)
+                {
+                    minPeso = peso;
+                }
+            }
+        }    
+        if (temperatura <0)
+        {
+            temperaturaMenorCero++
+        }   
+    
+    contador++;
+    respuesta = confirm ("desea continuar?");
+    }while (respuesta);
+acumuladorPeso = acumuladorPeso/contador;
+console.log("Temperaturas pares: "+ temperaturaPar);
+console.log("Marca producto mas pesado: " +marcaMasPesada);
+console.log("Productos conservados a -0: "+ temperaturaMenorCero);
+console.log("Promedio peso productos: " +acumuladorPeso);
+console.log("Peso maximo: "+ maxPeso);
+ console.log("Peso minimo: "+ minPeso);
+    
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,56 +1,92 @@
 var respuesta = true;
 var numero;
 var letra;
-var numerosPares=0;
-var numerosImpares =0;
+var numerosPares = 0;
+var numerosImpares = 0;
 var contadorCeros = 0;
-var promedioPositivos;
-var SumaPositivos;
-var contadorPositivos;
-var sumaNegativos=0;
+var promedioPositivos = 0;
+var contadorPositivos = 0;
+var sumaNegativos = 0;
+var min;
+var max;
+var letraMenor;
+var letraMayor;
+var contador=true;//para asignar max y min
+var acumulador;
 
 function mostrar()
 {
 do
 {
-{
+
     do
     {
         numero = prompt("Ingrese un numero");
         numero = parseInt(numero);
-    }   while(isNaN(numero && numero >-100 && numero<100 ));
+    }   while(isNaN(numero) || numero >-100 || numero<100 );
     
     do
     {
         letra = prompt("Ingrese una letra");
-
     }   while(!isNaN(letra));
-    if (numero %2==0)
+
+    if (numero %2==0 && numero!=0 ) //punto 1
     {
         numerosPares++;
     }
-    else
+    else //punto 2 tengo que sacar el 0 de los impares
     {
-        numerosImpares++;
+        if (numero %2!=0 && numero!=0)
+        numerosImpares++;   
     }
-    if (numero ==0)
+    if (numero == 0) //punto 3
     {
         contadorCeros++;
     }
-    if (numero >0)
+    else
     {
-        SumaPositivos +=numero;
+        if (numero >0) //punto 4
+        {
+            promedioPositivos +=numero;
+            contadorPositivos++;
+        }
+        else //punto 5
+        {
+            sumaNegativos+=numero;
+        }
+    }
+    if (contador)//punto 6
+    {
+        min = numero;
+        max = numero;
+        letraMayor = letra;
+        letraMenor = letra;
+        contador=false;
     }
     else
     {
-        sumaNegativos+=numero;
+        if (numero >max)
+        {
+            max = numero;
+            letraMayor = letra;
+        }
+        else
+        {
+            min = numero;
+            letraMenor = letra;   
+        }
     }
+acumulador = promedioPositivos/contadorPositivos;
 
-}
 respuesta = confirm("Deseas continuar?");
 }while (respuesta);
+
 console.log("Numeros pares  "+numerosPares);
 console.log("Numeros impares "+ numerosImpares);
 console.log("Cantidad de 0 = "+contadorCeros);
-console.log()
+console.log("Promedio de numeros positivos "+acumulador);
+console.log("Suma de negativos = " +sumaNegativos);
+console.log("El numero maximo es "+max +" y su letra es "+letraMayor);
+console.log("El numero minimo es "+min+" y su letra es "+letraMenor);
 }
+
